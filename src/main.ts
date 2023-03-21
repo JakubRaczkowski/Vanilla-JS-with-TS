fdbefd9d94a57c50890249589e9c3c311caa24b8
unknown <rakolbn@gmail.com>
2023-03-21 14:56:12 +0100

    finished exc

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import './css/style.css'
+import FullList from './model/FullList'
+import ListTemplate from './templates/ListTemplate'
+import ListItem from './model/ListItem'
+
+const initApp = (): void => {
+
+  const fullList = FullList.instance
+  const listTemplate = ListTemplate.instance
+
+  const itemEntryForm =document.getElementById('itemEntryForm') as HTMLFormElement
+  itemEntryForm.addEventListener('submit', (e: SubmitEvent) : void => {
+    e.preventDefault()
+
+    const input = document.getElementById('newItem') as HTMLInputElement
+    const newEntryText: string = input.value.trim()
+    if(newEntryText.length === 0) return
+    
+    const itemId:number = fullList.list.length ?
+     parseInt( fullList.list[fullList.list.length - 1].id)+ 1 : 1
+
+    const newItem = new ListItem(itemId.toString(), newEntryText)
+
+    fullList.addItem(newItem)
+    listTemplate.render(fullList)
+
+  })
+
+  const clearItems = document.getElementById('clearItemsButton') as HTMLButtonElement
+  clearItems.addEventListener('click', (e: MouseEvent) : void => {
+    fullList.clearList()
+    listTemplate.clear();
+    
+  })
+
+  fullList.load()
+  listTemplate.render(fullList);
+
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
+
